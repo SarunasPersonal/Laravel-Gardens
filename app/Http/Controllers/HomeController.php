d4c84d972a4b68c3864a6b718b7efee16adfcc17
Sarunas Slekys <52355769+SarunasPersonal@users.noreply.github.com>
2024-06-08 20:17:06 +0100

    "Admin dash for admin view was added"

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+//auth was added to check if the user is authenticated or not
+use Illuminate\Support\Facades\Auth;
+//app model user was added to check the usertype of the user
+use App\Models\User;
+
+class HomeController extends Controller
+{
+    //it will check if the user is authenticated or not
+    public function redirect()
+    {
+        $usertype=Auth::user()->usertype;
+        //if user is admin then it will redirect to admin.home page
+        if ($usertype=='1') {
+            return view('admin.home');
+
+        } 
+        else 
+        {
+            return view('dashboard');
+        }
+    }
+}
---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,13 @@
+<x-app-layout>
+  
+</x-app-layout>
+<!DOCTYPE html>
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+</head>
+<body>
+admin dashboard
+</body>
+</html>
