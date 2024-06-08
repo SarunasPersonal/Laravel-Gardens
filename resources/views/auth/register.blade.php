ef562682039fd6336190aea067fb2d166e5ea18e
Sarunas Slekys <52355769+SarunasPersonal@users.noreply.github.com>
2024-06-08 18:09:18 +0100

    "Fully Working Login and register form"

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -18,6 +18,14 @@
                 <x-label for="email" value="{{ __('Email') }}" />
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
             </div>
+            <div class="mt-4">
+                <x-label for="phone" value="{{ __('Phone Number') }}" />
+                <x-input id="phone" class="block mt-1 w-full" type="number" name="phone" :value="old('phone')" required autocomplete="phone" />
+            </div>
+            <div class="mt-4">
+                <x-label for="address" value="{{ __('Address') }}" />
+                <x-input id="address" class="block mt-1 w-full" type="text" name="address" :value="old('address')" required autocomplete="username" />
+            </div>
 
             <div class="mt-4">
                 <x-label for="password" value="{{ __('Password') }}" />
