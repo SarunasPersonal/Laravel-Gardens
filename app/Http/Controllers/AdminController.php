4944837eadfc9f191af45031cea8debe3c507b06
Sarunas Slekys <52355769+SarunasPersonal@users.noreply.github.com>
2024-06-09 16:33:27 +0100

    "Comments added"

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -12,14 +12,24 @@ class AdminController extends Controller
     {
         return view('admin.category');
     }
+    /**
+     * Add a new category.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function add_category(Request $request)
-    //add category function
     {
-        $data=new category;
-        $data->category_name=$request->category;
+        // Create a new instance of the category model
+        $data = new category;
+        
+        // Set the category name from the request
+        $data->category_name = $request->category;
 
+        // Save the category to the database
         $data->save();
-        return redirect()->back();
 
+        // Redirect back to the previous page
+        return redirect()->back();
     }
 }
