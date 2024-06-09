d9fd7dfb0fdfe4db34cbe1bfd9d77d2cf53cb76e
Sarunas Slekys <52355769+SarunasPersonal@users.noreply.github.com>
2024-06-09 16:02:46 +0100

    "Add button in Categories"

--- resources/views/admin/category.blade.php
+++ resources/views/admin/category.blade.php
@@ -1,22 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <!-- start plugin css for this page -->
+    <!-- Required meta tags -->
     @include('admin.css')
-    
+    <!-- End layout styles -->
+    <link rel="shortcut icon" href="admin/assets/images/favicon.png" />
+    <style>
+      .div_center {
+        text-align: center;
+        padding: 80px;
+      }
+      .h2_font {
+        font-size: 40px;
+        padding-bottom: 40px;
+      }
+      .input_color{
+        color:black;
+      }
+      </style>
+  </head>
+  <body>
+    <div class="container-scroller">
       <!-- partial:partials/_sidebar.html -->
       @include('admin.sidebar')
       <!-- partial -->
-      
+      <div class="container-fluid page-body-wrapper">
         <!-- partial:partials/_navbar.html -->
-    @include('admin.navbar')
-        
+       @include('admin.navbar')
+        <!-- partial -->
+        <div class="main-panel">
+          <div class="content-wrapper">
+            <div class="div_center">
+              <h2 class="h2_font">Add Category</h2>
+              <form>
+                <input class="input_color" type="text" name="category" placeholder="Enter Category name" required>
+                <input type="submit" name="submin" class="btn btn-primary" value="Add Category">
+              </form>
+            </div>
+          </div>
+       </div>
+      
           <!-- content-wrapper ends -->
-          
           <!-- partial:partials/_footer.html -->
-    @include('admin.footer')
-    
+          @include('admin.footer')
+          <!-- partial -->
+        </div>
+        <!-- main-panel ends -->
+      </div>
+      <!-- page-body-wrapper ends -->
+    </div>
     <!-- container-scroller -->
-    @include('admin.script')
-    <!-- End custom js for this page -->
   </body>
