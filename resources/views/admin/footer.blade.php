3b4035d986120dcf2f7efa99fe208c997197c642
Sarunas Slekys <52355769+SarunasPersonal@users.noreply.github.com>
2024-06-09 14:14:28 +0100

    "Admin Dashboard added" "working login log out option was implamented"

---
+++ resources/views/admin/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="footer">
+    <div class="d-sm-flex justify-content-center ">
+      <span class="text-muted d-block text-center d-sm-inline-block">Copyright © Broadleigh Gardens</span>
+      <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"></span>
+    </div>
+  </footer>
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,12 +1,28 @@
-<x-app-layout>
-</x-app-layout>
 <!DOCTYPE html>
-<head>
-<meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title></title>
-</head>
-<body>
-admin dashboard
-</body>
-</html>
+<html lang="en">
+  <head>
+    <!-- start plugin css for this page -->
+    @include('admin.css')
+    
+      <!-- partial:partials/_sidebar.html -->
+      @include('admin.side_bar')
+      <!-- partial -->
+      <div class="container-fluid page-body-wrapper">
+        <!-- partial:partials/_navbar.html -->
+        @include('admin.navbar')
+        
+          <!-- content-wrapper ends -->
+          @include('admin.main_panel')
+          <!-- partial:partials/_footer.html -->
+          @include('admin.footer')
+          <!-- partial -->
+        </div>
+        <!-- main-panel ends -->
+      </div>
+      <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    @include('admin.script')
+    <!-- End custom js for this page -->
+  </body>
+</html>admin/
